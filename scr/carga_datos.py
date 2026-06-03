a190b871cc86ac990cfb1c23bbd128d6174c2410
anapiuma-ship-it <mpiumaisaurralde@udesa.edu.ar>
2026-06-03 13:31:48 -0300

    archvio Carga_datos
    codigo que carga los datos del archivo csv

--- scr/carga_datos.py
+++ scr/carga_datos.py
@@ -6,3 +6,28 @@ Created on Wed Jun  3 13:28:31 2026
 @author: catalinabellomo
 """
 
+import pandas as pd
+
+def cargar_dataset(ruta):
+    """
+    Carga el dataset desde un archivo CSV. 
+
+    Parámetros:
+        ruta (str): ubicacion del archivo.
+
+    Retorna:
+        DataFrame: dataset cargado en pandas
+    """
+    try:
+        datos = pd.read_csv(ruta)
+        return datos
+
+    except FileNotFoundError:
+        print("No se encontró el archivo.")
+        return None
+
+    except Exception as error:
+        print("Error:", error)
+        return None
+    
+    
